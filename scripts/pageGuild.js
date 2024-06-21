4b961468407d8db9ec8a3ccee47ad930dbc697ca
Wily CV <bestcodecv@gmail.com>
2024-06-21 15:20:44 -0500

    ultimo

---
+++ scripts/pageGuild.js
@@ -0,0 +1,30 @@
+import { User } from './models/user.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('./scripts/data/guilds.json')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            //const users = data.map(user => new User(user.id, user.name, user.age));
+            //renderUsersTable(users);
+        })
+        .catch(error => console.error('Error loading JSON data:', error));
+});
+
+function renderUsersTable(users) {
+    const tableBody = document.querySelector('#data-table tbody');
+    tableBody.innerHTML = '';
+    users.forEach(user => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${user.id}</td>
+            <td>${user.name}</td>
+            <td>${user.age}</td>
+        `;
+        row.addEventListener('click', () => {
+            window.location.href = `user-details.html?id=${user.id}`;
+            console.log("clikado")
+        });
+        tableBody.appendChild(row);
+    });
+}
